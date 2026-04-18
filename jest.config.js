f235a12dad24456be5bb4ae0921bc3567638c6ab
MyGigsters Dev Team <dev@mygigsters.com.au>
2026-04-18 11:20:47 +1000

    fix: Jest only runs tests/ dir, all src/ tests handled by Vitest

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/tests', '<rootDir>/src'],
-  testMatch: ['**/tests/**/*.test.ts', '**/src/providers/__tests__/**/*.test.ts'],
-  testPathIgnorePatterns: ['/node_modules/', '/src/__tests__/'],
+  testMatch: ['**/tests/**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/src/'],
   moduleFileExtensions: ['ts', 'js', 'json'],
 };
